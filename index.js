4c6262c4d482fe2b2b6b91d437c1f5edd690abfc
sushant4612 <sushantsp14@gmail.com>
2023-08-16 17:23:04 +0530

    Added menu option for small screen

---
+++ index.js
@@ -0,0 +1,18 @@
+function myFunction(x) {
+  x.classList.toggle("change");
+}
+let navbar = document.querySelector('.navbar');
+let menu = document.querySelector('.menu');
+let moto = document.querySelector('.moto');
+let change = document.querySelector('.change');
+let button = document.querySelector('.header > button');
+
+menu.addEventListener('click',() => {
+  navbar.style.display = navbar.style.display == 'flex' ? 'none' : 'flex';
+  button.style.display = button.style.display == 'flex' ? 'none' : 'flex';
+  if (navbar.style.display == 'flex'){
+      moto.style.display = 'none';
+  }else{
+      moto.style.display = 'block'
+  }
+})
